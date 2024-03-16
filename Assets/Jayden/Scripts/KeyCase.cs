882fecff0f9688cdfa38d5ee2a711bb12cbb7511
JayIsPOG <163608662+JayIsPOG@users.noreply.github.com>
2024-03-16 19:09:14 -0400

    Made key

---
+++ Assets/Jayden/Scripts/KeyCase.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCase : MonoBehaviour
+{
+    public bool activated, keyPickedUp;
+    public GameObject puzzleElement, key;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(!keyPickedUp)
+        {
+            if(puzzleElement.name.Contains("Push Button"))
+            {
+                activated = puzzleElement.GetComponent<PushButton>().pushed;
+            }
+        }else{
+            activated = true;
+        }
+
+        if(activated)
+        {
+            GetComponent<Animator>().SetBool("Open", true);
+        }else{
+            GetComponent<Animator>().SetBool("Open", false);
+        }
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.transform.tag == "Hands" && activated)
+        {
+            keyPickedUp = true;
+            key.SetActive(false);
+        }
+    }
+}
