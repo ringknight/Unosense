43bbf26f750adde25c69e2f291b75c804de053cd
JayIsPOG <163608662+JayIsPOG@users.noreply.github.com>
2024-03-15 22:48:41 -0400

    Made a script for a push button that doesn't work

---
+++ Assets/Jayden/Scripts/PushButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushButton : MonoBehaviour
+{
+    public bool pushed, toggle;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        //if (collision.gameObject.transform.tag == "Hands")
+        //{
+            //pushed = !pushed;
+            if(toggle)
+            {
+                pushed = !pushed;
+            }
+            else
+            {
+                pushed = true;
+            }
+            
+        //}
+    }
+}
