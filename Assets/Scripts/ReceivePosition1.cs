4306cced63332d727d4bd999b51d6938c8cb22d6
JayIsPOG <163608662+JayIsPOG@users.noreply.github.com>
2024-03-16 11:52:29 -0400

    Créé une nouvelle scène pour tester

---
+++ Assets/Scripts/ReceivePosition1.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReceivePosition1 : MonoBehaviour {
+    
+   	public OSC osc;
+    public GameObject body;
+    public string adressPrefix;
+
+
+	// Use this for initialization
+	void Start () {
+	   osc.SetAddressHandler(adressPrefix + "/x", SetBodyX);
+       osc.SetAddressHandler(adressPrefix + "/y", SetBodyY);
+       osc.SetAddressHandler(adressPrefix + "/z", SetBodyZ);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SetBodyX(OscMessage message){
+		float x = message.GetFloat(0);
+
+        Vector3 position = transform.position;
+
+        position.x = x / 100.0f;
+
+        transform.position = position;
+	}
+    void SetBodyY(OscMessage message){
+		float y = message.GetFloat(0);
+
+        Vector3 position = transform.position;
+
+        position.y = y / 100.0f;
+
+        transform.position = position;
+	}
+    void SetBodyZ(OscMessage message){
+		float z = message.GetFloat(0);
+
+        Vector3 position = transform.position;
+
+        position.z = z / 100.0f;
+
+        transform.position = position;
+	}
+}
